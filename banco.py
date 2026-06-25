fa5a36b82ad18cde240cd047025dbbe669c0f7c6
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-06-25 20:18:49 +0900

    feat: commit-04

--- banco.py
+++ banco.py
@@ -1,3 +1,6 @@
 print('bem-vindo ao banco python.')
 salario_mensal = float(input('qual seu salario mensal?: '))
 idade = int(input('sua idade?: '))
+
+if idade < 18:
+    print('empréstimo negado por idade')
